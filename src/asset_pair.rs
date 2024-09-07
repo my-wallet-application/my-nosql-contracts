a89e9eaa091a18668dd785c005dcade2283b2dfe
amigin <amigin@gmail.com>
2024-09-07 23:45:50 +0300

    Added AssetPairMyNoSqlEntity

---
+++ src/asset_pair.rs
@@ -0,0 +1,31 @@
+service_sdk::macros::use_my_no_sql_entity!();
+use serde::*;
+
+#[my_no_sql_entity("asset-pairs")]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct AssetPairMyNoSqlEntity {
+    #[serde(rename = "Direct")]
+    pub direct: ConvertData,
+    #[serde(rename = "Reverse")]
+    pub reverse: ConvertData,
+    #[serde(rename = "PriceAccuracy")]
+    pub price_accuracy: usize,
+    #[serde(rename = "IsEnabled")]
+    pub is_enabled: bool,
+}
+
+impl AssetPairMyNoSqlEntity {
+    pub const PARTITION_KEY: &'static str = "c";
+
+    pub fn get_id(&self) -> &str {
+        &self.row_key
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ConvertData {
+    #[serde(rename = "FromAsset")]
+    pub from_asset: String,
+    #[serde(rename = "ToAsset")]
+    pub to_asset: String,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,5 @@ mod key_value;
 pub use key_value::*;
 mod asset_entity;
 pub use asset_entity::*;
+mod asset_pair;
+pub use asset_pair::*;
