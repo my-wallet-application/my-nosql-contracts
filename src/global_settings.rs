abd67be6fc600261bcf2dc8216d3eab09793efdd
amigin <amigin@gmail.com>
2024-10-10 22:10:36 +0300

    Changed to corporate_account_id

--- src/global_settings.rs
+++ src/global_settings.rs
@@ -5,7 +5,7 @@ service_sdk::macros::use_my_no_sql_entity!();
 #[my_no_sql_entity("global-settings")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct GlobalSettingsMyNoSqlEntity {
-    pub commissions_account_id: String,
+    pub corporate_account_id: String,
 }
 
 impl GlobalSettingsMyNoSqlEntity {
