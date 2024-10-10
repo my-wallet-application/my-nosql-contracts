bd0d2f5f8a297d932e4208524ebb47e99e598e8f
amigin <amigin@gmail.com>
2024-10-10 21:56:34 +0300

    GlobalSettingsMyNoSqlEntity

--- src/global_settings.rs
+++ src/global_settings.rs
@@ -4,6 +4,6 @@ service_sdk::macros::use_my_no_sql_entity!();
 
 #[my_no_sql_entity("global-settings")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct GlobalSettings {
+pub struct GlobalSettingsMyNoSqlEntity {
     pub commissions_account_id: String,
 }
