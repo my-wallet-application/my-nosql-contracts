45a8fd90cda3bf489718e07cdec5a8bd96f8b33b
yuriy-smplt <pluriw@gmail.com>
2025-01-21 15:08:26 +0200

    added send-grid

--- src/send_grid_settings.rs
+++ src/send_grid_settings.rs
@@ -7,6 +7,18 @@ service_sdk::macros::use_my_no_sql_entity!();
 #[serde(rename_all = "PascalCase")]
 pub struct SendGridSettingsMyNoSqlEntity {
     pub send_grid_api_key: String,
+    pub email_from: String,
+    pub email_from_name: Option<String>,
+    pub email_cc: Option<String>,
+    pub email_bcc: Option<String>,
+    pub mail_logo_url: Option<String>,
+    pub mail_success_picture_url: Option<String>,
+    pub mail_fail_picture_url: Option<String>,
+    pub policy_url: String,
+    pub terms_url: String,
+    pub about_url: String,
+    pub support_url: String,
+    pub home_page_url: String,
 }
 
 impl SendGridSettingsMyNoSqlEntity {
